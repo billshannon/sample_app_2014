7b53cbb9faeab8a479a1eee6c292a4daf6ebafe5
Bill Shannon <bill@billshannon.com>
2015-01-06 21:17:59 -0700

    add test layout changes

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,10 +2,14 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:bill)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, session: { email: "", password: "" }
+    post login_path, session: {email: "", password: ""}
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
